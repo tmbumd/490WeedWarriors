4d869d83b7ee7504422a99bc9f6fe613453bdbef
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-04-04 19:27:54 -0400

    Updated form UI

--- client/script.js
+++ client/script.js
@@ -1,5 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // UIkit.formCustom(element);
+document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('select');
+    let instances = M.FormSelect.init(elems);
 
-});
+
+    const plantImageContainer = document.querySelector('#plant-img')
+    const selectedPlant = document.querySelector('#plant-select')
+    selectedPlant.addEventListener('change', function() {
+        plantImageContainer.src = `images/${selectedPlant.value}.jpg`
+        console.log(`images/${selectedPlant.value}.jpg`)
+    })
+
+    const coordinates = document.querySelector('#coordinates')
+    navigator.geolocation.getCurrentPosition(function(position) {
+        coordinates.value = `[${position.coords.latitude}, ${position.coords.longitude}]`
+        // console.log(`[${position.coords.latitude}, ${position.coords.longitude}]`)
+    })
+
+
+  });
+
 
