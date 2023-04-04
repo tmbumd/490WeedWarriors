c5fae84ef508b39d34c250cd864f04c3a081cf9d
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-04-04 19:52:02 -0400

    Change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import apiRoutes from './server/routes/apiRoutes.js';
 
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 3000;
 const staticFolder = 'client';
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
 
-app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
 });
