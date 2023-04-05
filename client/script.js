fd388c25261b6f57f605fa7cbeff123a2b5561ce
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-04-04 20:10:24 -0400

    Update form styling

--- client/script.js
+++ client/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const elems = document.querySelectorAll('select');
+    let elems = document.querySelectorAll('select');
     let instances = M.FormSelect.init(elems);
+    elems = document.querySelectorAll('.materialboxed');
+    instances = M.Materialbox.init(elems);
 
 
     const plantImageContainer = document.querySelector('#plant-img')
