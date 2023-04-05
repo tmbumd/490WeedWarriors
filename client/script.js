c5108da0b3ed9d8d0c5f7b71877eba24535ba360
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-04-04 20:43:10 -0400

    Update form UI

--- client/script.js
+++ client/script.js
@@ -3,6 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
     let instances = M.FormSelect.init(elems);
     elems = document.querySelectorAll('.materialboxed');
     instances = M.Materialbox.init(elems);
+    // let z = document.querySelectorAll('.autocomplete');
+    // let x = M.Autocomplete.init(z, {
+    //    data: {
+    //       "Indian mallow [Abutilon]": 'images/003.jpg',
+    //       "Balsam fir [Abies balsamea]": 'images/002.jpg',
+    //       "Okra [Abelmoschus esculentus]": 'images/001.jpg'
+    //       },
+    //    minLength: 0, 
+    // });
+
+
+    // const plantImageContainer = document.querySelector('#plant-img')
+    // const selectedPlant = document.querySelector('#autocomplete-input')
+    // selectedPlant.addEventListener('change', function() {
+    //     // plantImageContainer.src = `images/${selectedPlant.value}.jpg`
+    //     // console.log(`images/${selectedPlant.value}.jpg`)
+    //     // console.log(selectedPlant.value)
+    //     console.log(x.data)
+    // })
 
 
     const plantImageContainer = document.querySelector('#plant-img')
