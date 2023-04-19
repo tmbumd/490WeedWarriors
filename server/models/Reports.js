b3aefe324642431df9e45c37b3f7b878d23434f1
nowamey <nowamey100@gmail.com>
2023-04-19 18:37:50 -0400

    finishedreporttable

--- server/models/Reports.js
+++ server/models/Reports.js
@@ -7,6 +7,39 @@ export default (database, DataTypes) => {
                 allowNull: false,
                 primaryKey: true
             },
+            timestamp: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            catalog_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            location: {
+                type: DataTypes.GEOMETRY('POLYGON'),
+                allowNull: false
+            },
+            severity_id:{   
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            media_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            customElements: {
+                type: DataTypes.STRING,
+                allowNull
+            },
+            person_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            verified:{
+                type: DataTypes.BOOLEAN,
+                allowNull: false
+            }
+
         },
         { freezeTableName: true, timestamps: false }
     );
