03b3583862200c6621ebeb07e84317a8c27dc40f
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-04-19 15:34:38 -0400

    Update dropdown options

--- client/script.js
+++ client/script.js
@@ -3,23 +3,15 @@ $('.ui.dropdown')
         {
             fullTextSearch: true,
             values: [{
-                "name": "Jenny",
-                "value": "jenny",
-                "image": "/images/001.jpg",
-                "imageClass": "ui mini avatar image"
+                "name": "Maidenhair fern [Adiantum]",
+                "value": "ADIAN"
             },
             {
-                "name": "Avatar1",
-                "value": "Avatar1",
-                "image": "/images/002.jpg",
-                "imageClass": "ui mini avatar image"
+                "name": "False foxglove [Agalinis]",
+                "value": "AGALI"
             },
             {
-                "name": "Avatar2",
-                "value": "Avatar2",
-                "image": "/images/003.jpg",
-                "imageClass": "ui mini avatar image"
+                "name": "Ladyfern [Athyrium]",
+                "value": "ATHYR"
             }]
         });
-
-        $( "input" ).autocomplete( "disable" );
