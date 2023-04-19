ecd11d6288fac09ae7a69de0e12a93a769b3063d
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-04-19 19:20:12 -0400

    Update routes

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -8,13 +8,44 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/catalog')
-  .get(async (req, res) => { 
+  .get(async (req, res) => {
     try {
       const catalog = await db.Catalog.findAll();
       const result = catalog.length > 0 ? { data: catalog } : { message: 'No results found' };
       res.json(result);
     } catch (err) {
       res.json('Server error');
+    }
+  });
+
+router.route('/reports')
+  .get(async (req, res) => {
+    try {
+      const reports = await db.Reports.findAll();
+      const result = reports.length > 0 ? { data: reports } : { message: 'No results found' };
+      res.json(result);
+    } catch (err) {
+      res.json('Server error');
+    }
+  })
+  .post(async (req, res) => {
+    const reports = await db.Reports.findAll();
+    const currentId = (await reports.length) + 1;
+    try {
+      await db.Reports.create({
+        report_id: currentId,
+        timestamp: req.timestamp,
+        catalog_id: req.catalog_id,
+        location: req.location, //CREATE THIS
+        severity_id: req.severity_id,
+        media_id: req.media_id, //CREATE THIS
+        comments: req.comments,
+        person_id: req.person_id, //FIND/CREATE THIS
+        verified: 0
+      });
+      res.send('Successfully added');
+    } catch (err) {
+      res.send(err);
     }
   });
 
